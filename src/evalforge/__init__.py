64b7f0bbbb22875636e2540c771da7e37ff7050b
Gaurav Kumar Sinha <gaurav@substrai.dev>
2026-05-30 01:09:56 -0500

    chore: bump version to 0.9.0 for PyPI release
    Signed-off-by: Gaurav Kumar Sinha <gaurav@substrai.dev>

--- src/evalforge/__init__.py
+++ src/evalforge/__init__.py
@@ -13,7 +13,7 @@ Usage:
     print(results.summary())
 """
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 from evalforge.core.config import EvalConfig, UseCaseType
 from evalforge.core.pipeline import EvalPipeline
